21c49d7e99a420e2532599a95486c99f5f7d7c71
Prashant Shrestha <prashantlurvs@gmail.com>
2020-05-19 12:07:56 +0545

    Initial Commit

---
+++ CleanyModalView/CleanyModalView.h
@@ -0,0 +1,19 @@
+//
+//  CleanyModalView.h
+//  CleanyModalView
+//
+//  Created by Prashant Shrestha on 5/19/20.
+//  Copyright © 2020 Inficare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CleanyModalView.
+FOUNDATION_EXPORT double CleanyModalViewVersionNumber;
+
+//! Project version string for CleanyModalView.
+FOUNDATION_EXPORT const unsigned char CleanyModalViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanyModalView/PublicHeader.h>
+
+
---
+++ Sources/CleanyModal/Model/AlertModel.swift
@@ -0,0 +1,18 @@
+//
+//  AlertModel.swift
+//  CleanyModal
+//
+//  Created by Lory Huz on 07/01/2020.
+//
+
+import UIKit
+
+struct AlertModel {
+    
+    var title: String?
+    var attributedTitle: NSAttributedString?
+    var attributedMessage: NSAttributedString?
+    var message: String?
+    var iconName: String?
+    
+}
